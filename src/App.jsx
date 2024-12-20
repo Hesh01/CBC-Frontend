0cd6a45c78d207121e8f9cdb7e3eb6b66071f337
Heshani Narmada <heshaninarmada57@gmail.com>
2024-12-20 15:38:04 +0530

    add pages and some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { LoginPage } from './assets/Pages/LoginPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Homepage } from './assets/Pages/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div className="text-red-600">
-      Mern App
-     </div>
+      <BrowserRouter>
+        <Routes path="/*">
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/*" element={<Homepage/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
